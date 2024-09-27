c117797a303e50454f50b85c55e50111ce8cd845
lapazyygeli <137160486+lapazyygeli@users.noreply.github.com>
2024-09-27 21:11:35 +0300

    Fix merge conflicts

--- hello_world.py
+++ hello_world.py
@@ -1,10 +1,4 @@
 print("Hello world!")
-<<<<<<< HEAD
-<<<<<<< HEAD
-print("Hello once again")
-=======
-print("I've come to talk with you again")
->>>>>>> intermediate-merge-conflict/feature/modify-print
-=======
 print("Hello darkness my old friend")
->>>>>>> intermediate-merge-conflict/master
+print("I've come to talk with you again")
+
